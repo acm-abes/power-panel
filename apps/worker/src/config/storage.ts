caba4adf3339297d97e7eb0983e8824810927ed5
Kunal Rana <kunalranarj2005@gmail.com>
2026-02-04 22:37:15 +0530

    added storage package

---
+++ apps/worker/src/config/storage.ts
@@ -0,0 +1,28 @@
+/** @format */
+
+import { z } from "zod";
+
+const envSchema = z.object({
+  S3_BUCKET: z.string(),
+  S3_REGION: z.string(),
+  S3_PREFIX: z.string(),
+  STORAGE_PROVIDER: z.string(),
+});
+
+/**
+ * Validate environment variables based on the selected mail provider
+ */
+function validateEnv() {
+  const result = envSchema.safeParse(process.env);
+
+  if (!result.success) {
+    console.error("Environment validation error:", result.error.format());
+    throw new Error("Invalid environment variables");
+  }
+
+  return result.data;
+}
+
+export const storageEnv = validateEnv();
+
+export type StorageEnv = typeof storageEnv;
